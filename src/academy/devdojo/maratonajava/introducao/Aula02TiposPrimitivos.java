181a68e2303b78a60babe015bbc0004cd844d1de
Felipe <felipejesusamando@gmail.com>
2026-07-14 16:30:44 -0300

    Primeiro commit substituição false e true

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        long numeroGrande = 10000000000L;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = 127;
+        short idadeShort = 32767;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'F';
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Número Grande: " + numeroGrande);
+        System.out.println("Salário Double: " + salarioDouble);
+        System.out.println("Salário Float: " + salarioFloat);
+        System.out.println("Idade Byte: " + idadeByte);
+        System.out.println("Idade Short: " + idadeShort);
+        System.out.println("Verdadeiro: " + verdadeiro);
+        System.out.println("Falso: " + falso);
+        System.out.println("Caractere: " + caractere);
+    }
+
+}
