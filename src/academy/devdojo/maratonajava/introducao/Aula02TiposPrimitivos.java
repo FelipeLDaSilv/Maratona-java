fd5c4edc69e21e67defca73f1b61e0bb6eb7b114
Felipe Linhares <felipejesusamando@gmail.com>
2026-07-15 19:55:54 -0300

    testando o git na maquina secundaria para o projeto

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,6 +3,7 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
+        // Aula 02 - Tipos Primitivos
         int idade = 10;
         long numeroGrande = 10000000000L;
         double salarioDouble = 2000.0D;
